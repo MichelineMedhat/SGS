4e5687118c965452d34bd9cd90faf5023bc7e840
Micheline Medhat <MichelineMedhat@gmail.com>
2019-03-22 23:05:10 +0200

    Sign in page .Xaml

---
+++ SGS/SGS/MarkupExtenstions/EmbeddedImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SGS
+{
+    [ContentProperty("ReSourceID")]
+    class EmbeddedImage : IMarkupExtension
+    {
+        public string ReSourceID { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (string.IsNullOrWhiteSpace(ReSourceID))
+                return null;
+
+            return ImageSource.FromResource(ReSourceID);
+
+        }
+    }
+}
